378f40ae21dea5458f91f2461b6dd11c5a087d20
Abhinay Singh <manish.singh@dbalounge.com>
2024-04-16 11:32:37 +0530

    form get data

--- controller/master_department.controller.js
+++ controller/master_department.controller.js
@@ -2,19 +2,17 @@ const express = require('express');
 const router = express.Router();
 const Department = require('../models/department');
 
-const departmentMaster = async (req, res) => {
+const DesignationMaster = async (req, res) => {
   try {
-    const { dept_mr_dept_code, dept_mr_dept_short_desc, dept_mr_dept_desc, dept_mr_po_desc } = req.body;
-    if (!dept_mr_dept_code) {
-        return res.status(400).json({ error: 'dept_mr_dept_code is required' });
-      }
-    const department = await Department.create({ dept_mr_dept_code, dept_mr_dept_short_desc, dept_mr_dept_desc, dept_mr_po_desc });
-    res.status(201).json(department);
+    const designations = await Department.findAll();
+
+    res.status(200).json(designations);
   } catch (error) {
-    console.error('Error inserting data:', error);
+    console.error('Error fetching data:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
-module.exports = { departmentMaster };
+
+module.exports = { DesignationMaster };
 
--- routes/private.routes.js
+++ routes/private.routes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const Department = require('../models/department');
 const masterController = require('../controller/master_department.controller');
 
-router.post("/departments", masterController.departmentMaster);
+router.get("/designation", masterController.DesignationMaster)
+
+
 
 module.exports = router;
 
